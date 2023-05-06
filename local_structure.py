d7a21284c1e7e020c03c0ed7e66a22a3e5c14f47
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-05-06 10:06:45 +0530

    multi hop implemented

--- local_structure.py
+++ local_structure.py
@@ -66,18 +66,37 @@ def old_get_local_model(feat_info, upsampling=False):
     return customized_GAT(feat_info['t_feat'][1], feat_info['t_feat'][1], retatt=True)
 
 
-def get_graph_local_structure(graph, feats):
+
+def get_upsampling_model(feat_info):
+    '''upsampling the features of a graph
+    '''
+    return customized_GAT(feat_info['s_feat'][1],feat_info['t_feat'][1])
+
+
+def get_graph_local_structure(graph, feats, multi_hop=2):
         with graph.local_scope():
             feats = feats.view(-1, 1, feats.shape[1])
-            graph.ndata.update({'ftl': feats, 'ftr': feats})
-            graph.apply_edges(fn.u_sub_v('ftl', 'ftr', 'diff'))
+            
+            graph.ndata["aproximation"] = feats
+            graph.ndata["degree"] = torch.ones(feats.shape)
+            for _ in range(multi_hop):
+                graph.update_all(message_avarage, reduce_avarage)
+            feats_mod = graph.ndata['aproximation'] / graph.ndata['degree']
+
+            graph.ndata.update({'org': feats_mod})
+            graph.apply_edges(fn.u_sub_v('aproximation', 'org', 'diff'))   # checkout the order
             e = graph.edata.pop('diff')
             e = torch.exp( (-1.0/100) * torch.sum(torch.abs(e), dim=-1) )
             e = edge_softmax(graph, e)
         return e
 
 
-def get_upsampling_model(feat_info):
-    '''upsampling the features of a graph
-    '''
-    return customized_GAT(feat_info['s_feat'][1],feat_info['t_feat'][1])
+def message_avarage(edge_batch):
+    src_feats = edge_batch.src['aproximation']
+    degree_feats = edge_batch.src['degree']
+    return {"n_r": src_feats, "degree": degree_feats}
+
+def reduce_avarage(node_batch):
+    new_aproximation = node_batch.mailbox['n_r'].sum(1)
+    new_degree = node_batch.mailbox['degree'].sum(1)
+    return {"aproximation": new_aproximation, "degree": new_degree}
